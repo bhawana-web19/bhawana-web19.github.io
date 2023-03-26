fc3f59b315256dd0189be145450d06266070faf9
bhawana-web19 <bhawanachadda123@gmail.com>
2023-03-27 00:09:55 +0530

    created using bootstrap

---
+++ assets/javascript/index.js
@@ -0,0 +1,3 @@
+function downloadResume(){
+    window.open("../assets/images/fw19_0604-Bhawana-Chadda-Resume.pdf","_blank")
+}
--- index.js
+++
@@ -1,3 +0,0 @@
-function downloadResume(){
-    window.open("./images/fw19_0604-Bhawana-Chadda-Resume.pdf","_blank")
-}
