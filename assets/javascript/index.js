59b66f64359b8e7e3231ce300f4f1072ca018038
bhawana-web19 <bhawanachadda123@gmail.com>
2023-03-29 02:41:31 +0530

    created again -more responsive now

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,14 @@
 function downloadResume(){
     window.open("../assets/images/fw19_0604-Bhawana-Chadda-Resume.pdf","_blank")
 }
+
+
+// close nav 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show")
+    })
+})
